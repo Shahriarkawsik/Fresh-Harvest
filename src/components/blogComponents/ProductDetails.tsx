2ea8ee4a3c77287753d31e2ef8b47c9f9658c16c
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-05-29 15:01:52 +0600

    make responsive

--- src/components/blogComponents/ProductDetails.tsx
+++ src/components/blogComponents/ProductDetails.tsx
@@ -6,6 +6,9 @@ import { Rating } from "react-simple-star-rating";
 
 export default function ProductDetails() {
   const [quantity, setQuantity] = useState<number>(1);
+  {
+    /* TODO: make it dynamic rating */
+  }
   // const [rating, setRating] = useState<number>(5);
   const rating: number = 5;
   // const handleRating = (rate: number) => {
@@ -22,7 +25,7 @@ export default function ProductDetails() {
   };
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-20">
-      <figure className="w-full">
+      <figure className="w-ful">
         <Image
           src="/assets/coconut.png"
           width={500}
@@ -31,17 +34,17 @@ export default function ProductDetails() {
           className="w-full h-full rounded-3xl"
         />
       </figure>
-      <div className="space-y-10 font-rubik">
-        <div className="space-y-6">
+      <div className="space-y-10 font-rubik flex flex-col justify-between py-5">
+        <div className="max-sm:space-y-6 max-lg:space-y-8 max-2xl:space-y-10 2xl:space-y-10">
           <button className="text-[14px] sm:text-xl bg-[#749B3F1A] rounded-xl font-medium text-greenText px-3 py-1 ">
             Fruits
           </button>
           <h2 className="text-[#212337] text-3xl sm:text-5xl font-medium ">
             Coconut
           </h2>
-          {/* TODO: */}
+
           <div className="flex items-center gap-5">
-            <Rating initialValue={rating} size={20}/>
+            <Rating initialValue={rating} size={20} />
             <p className="font-medium text-[18px]">
               {rating.toFixed(1)}{" "}
               <span className="text-[12px]">(1 review)</span>{" "}
@@ -59,7 +62,7 @@ export default function ProductDetails() {
             dishes, or even for a refreshing drink straight from the shell.
           </p>
         </div>
-        <div className="space-y-6">
+        <div className="max-sm:space-y-6 max-lg:space-y-8 max-2xl:space-y-10 2xl:space-y-10">
           {/* Quantity */}
           <div className="flex gap-10 items-center">
             <span className="text-xl font-medium text-[#212337]">Quantity</span>
