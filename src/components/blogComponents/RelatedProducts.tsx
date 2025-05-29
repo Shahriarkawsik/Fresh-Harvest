194177bd9b9ae5ad0c5987410268cb0d106038a7
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-05-29 12:32:42 +0600

    shift my blog details product components

---
+++ src/components/blogComponents/RelatedProducts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function RelatedProducts() {
+  return (
+    <section>
+      {/* Header */}
+      <div>
+        <button className="text- text-xl bg-[#749B3F1A] rounded-xl font-medium text-greenText px-3 py-1 ">
+          Out Products
+        </button>
+      </div>
+    </section>
+  );
+}
